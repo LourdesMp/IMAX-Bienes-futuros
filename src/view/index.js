61c258dfa8d1045a6621ba59c25b93df3c05a0a0
LourdesMp <lourdes.merino@imax.com.pe>
2020-10-11 17:33:16 -0500

    uniendo componentes

--- src/view/index.js
+++ src/view/index.js
@@ -1,12 +1,12 @@
 /* eslint-disable import/no-cycle */
-import Ingreso from './ingreso.js';
-import Registro from './registro.js';
+import Login from './login.js';
+import Register from './register.js';
 import Diferent from './404.js';
 
 const components = {
   
-  ingreso: Ingreso, 
-  registro: Registro,
+  login: Login, 
+  register: Register,
   diferent: Diferent
 };
 
