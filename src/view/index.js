ef01a4e506b6781928caf122f067551e284a743d
LourdesMp <lourdes.merino@imax.com.pe>
2020-10-11 19:26:18 -0500

    vista profile

--- src/view/index.js
+++ src/view/index.js
@@ -1,12 +1,16 @@
 /* eslint-disable import/no-cycle */
 import Login from './login.js';
 import Register from './register.js';
+import Home from './home.js';
+import Profile from './profile.js';
 import Diferent from './404.js';
 
 const components = {
   
   login: Login, 
   register: Register,
+  home: Home,
+  perfil: Profile,
   diferent: Diferent
 };
 
---
+++ src/view/profile.js
@@ -0,0 +1,42 @@
+export default () => {
+    const viewProfile = `
+    <header>
+        <img class="logo-imax" src="assets/logo-imax.png">
+        <nav class= "menu">
+            <ul class="menubar">
+            <li>
+                <a class="profile" href="#/perfil">Perfil</a>
+            </li>
+            <li>
+                <a class ="fut-assets" href="#/bienes">Bienes Futuros</a>
+            </li>
+            <li>
+            <a class="logOut">Cerrar Sesion <i class="fas fa-sign-out-alt"></i></a>
+            </li>
+            </ul>
+        </nav>
+    </header>
+    <div class ="box-body">
+        <div class="box-profile">
+            <p class="title">Perfil del usuario</p>
+            <div class="box-user"> <label>Nombre: </label> <input class="nombre" type="text"  required>   </div>
+            <div class="box-user"> <label>Área: </label> 
+                <select name="area" className="select-modal" required >
+                <option value="emp">Evaluación y Monitoreo de Proyectos</option>
+                <option value="ie">Informes Especiales</option>
+                <option value="iad">Innovacion y analisis de data</option>
+                </select>  </div>  
+            <div class="box-user"> <label>Email: </label> <input class="user" type="email" required>  </div>     
+            <div class="box-user"> <label>Password: </label> <input class="user" type="password"  required>  </div>         
+            <div class="box-btn">
+            <button class= "btn-guardar"> Guardar</button>
+            <button class= "btn-cancelar"> Cancelar </button>
+            </div>
+        </div>
+    <div>
+    `;
+    const divElem = document.createElement('div');
+    divElem.className = 'form-profile';
+    divElem.innerHTML = viewProfile;
+    return divElem;
+  };
